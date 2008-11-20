28197b8a2811625bd4045af945d236426569337f
Jason.Trump <Jason.Trump@79fdf6e4-e71e-db45-8c0a-8b265bab67e9>
2008-11-20 19:27:35 +0000

    build out bulk load script
    git-svn-id: https://mtgisvn/common/modules/java/BehaviorTracking/trunk@36124 79fdf6e4-e71e-db45-8c0a-8b265bab67e9

--- utils/bin/load-events.sh
+++ utils/bin/load-events.sh
@@ -1,16 +1,39 @@
 #!/bin/sh
 
-echo Unpacking $1...
-zcat $1 | java -Xmx256m -jar bt-utils.jar > temp.xml 
+## function to echo a usage message and exit.
+function usage {
+	cat <<EOF
 
-echo Loading raw XML...
-sqlldr $2 control=db/etl/load_event_xml.ctl data=temp.xml
+Usage: $0 [db connection] [event log]
 
-echo Transforming XML...
-sqlplus $2 @db/etl/event_transformation.sql
+	Example:
 
-echo Cleaning up...
-rm temp.xml
+	> $0 user/pass@sid log.bxml.gz.20080101000000
 
-echo Done.
+EOF
+	exit 1
+}
 
+## check arg count.
+if [[ $# < 2 ]]; then
+	usage;
+fi
+
+## verify that second arg is an actual file.
+if [ ! -f $2 ]; then
+	echo
+	echo "\"$2\" does not exist or is not a regular file."
+	usage;
+fi
+
+UNPACK="zcat $2"
+TRANSFORM="java -jar bt-utils.jar -tool xslt -xsl etl/insert_events.xsl -split event"
+LOAD="sqlldr $1 control=etl/load_event_csv.ctl 2>load_progress.log"
+
+##stop the script on error
+set -e
+
+echo "Loading $2 into behavior tracking database at $1."
+echo "Start: " $(date)
+$UNPACK | $TRANSFORM | $LOAD
+echo "End: " $(date)
