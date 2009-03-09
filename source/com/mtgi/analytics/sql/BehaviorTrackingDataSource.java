0499211540ddf9866411fac4b1c87e9007ccecb8
jason.trump <jason.trump@79fdf6e4-e71e-db45-8c0a-8b265bab67e9>
2009-03-09 19:56:13 +0000

    abstract statement control logic into OO model, for performance boost when executing a large number of sql statements.
    git-svn-id: https://mtgisvn/common/modules/java/BehaviorTracking/trunk@53145 79fdf6e4-e71e-db45-8c0a-8b265bab67e9

--- source/com/mtgi/analytics/sql/BehaviorTrackingDataSource.java
+++ source/com/mtgi/analytics/sql/BehaviorTrackingDataSource.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -58,10 +59,8 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 	}
 	
 	private static final String findSqlArg(Object[] args) {
-		if (args != null)
-			for (Object o : args)
-				if (o instanceof String)
-					return (String)o;
+		if (args != null && args.length > 0 && args[0] instanceof String)
+			return (String)args[0];
 		return null;
 	}
 
@@ -78,12 +77,11 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 		 * Standard implementation of equals / hashCode for proxy handlers.  Returns a non-null result if
 		 * <code>method</code> is an identity check that can be handled here; null otherwise.
 		 */
-		protected Object invokeIdentity(Object proxy, Method method, Object[] args) throws Throwable {
+		protected final Object invokeIdentity(Object proxy, Method method, Object[] args) throws Throwable {
 			String op = method.getName();
 			if (op.equals("equals")) {
 				return (proxy == args[0] ? Boolean.TRUE : Boolean.FALSE);
-			}
-			else if (op.equals("hashCode")) {
+			} else if (op.equals("hashCode")) {
 				return new Integer(hashCode());
 			}
 			return null;
@@ -97,7 +95,7 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 		 * @return the value returned by the delegate
 		 * @throws any exception thrown trying to invoke the method.
 		 */
-		protected Object invokeTarget(Method method, Object[] args) 
+		protected final Object invokeTarget(Method method, Object[] args) 
 			throws Throwable
 		{
 			try {
@@ -154,12 +152,16 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 
 			//if the return value is a statement, wrap the statement for behavior tracking.
 			Class<?> type = method.getReturnType();
-			if (Statement.class.isAssignableFrom(type)) {
+			if (PreparedStatement.class.isAssignableFrom(type)) {
 				//for prepared statement, the SQL is provided when the statement is created.
-				//for other statements we get the exact SQL when the statement is executed.
 				String sql = findSqlArg(args);
+				//for other statements we get the exact SQL when the statement is executed.
 				ret = Proxy.newProxyInstance(BehaviorTrackingDataSource.class.getClassLoader(), 
-						new Class[]{ type }, new StatementHandler(this, ret, sql));
+						new Class[]{ type }, new PreparedStatementHandler(this, ret, sql));
+			} else if (Statement.class.isAssignableFrom(type)) {
+				//for other statements we get the exact SQL when the statement is executed.
+				ret = Proxy.newProxyInstance(BehaviorTrackingDataSource.class.getClassLoader(), 
+						new Class[]{ type }, new DynamicStatementHandler(this, ret));
 			}
 			
 			return ret;
@@ -167,20 +169,35 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 		
 	}
 
-	/** Causes a target Statement object to generate behavior tracking events when execute*() methods are called */
-	protected class StatementHandler extends HandlerStub {
+	/** Base invocation handler for instrumenting Statement objects with behavior tracking events. */
+	protected abstract class StatementHandler extends HandlerStub {
 	
 		private ConnectionHandler parent;
-		private String sql;
 		private EventDataElement batch;
-		private EventDataElement parameters = new EventDataElement("parameters");
 		
-		public StatementHandler(ConnectionHandler parent, Object target, String sql) {
+		public StatementHandler(ConnectionHandler parent, Object target) {
 			super(target);
-			this.sql = sql; //might be null.
 			this.parent = parent;
 		}
+		
+		/** 
+		 * notification that a statement has been added to the current batch.  Subclasses must implement this method
+		 * to add any useful parameter info to <code>batchData</code>.
+		 */
+		protected abstract void addBatch(EventDataElement batchData, Object[] args);
+		/**
+		 * notification that a non-batch statement has been executed.  Subclasses
+		 * must implement this method to add any useful parameter data to <code>event</code>.
+		 */
+		protected abstract void addExecuteParameters(BehaviorEvent event, Object[] args);
 
+		/**
+		 * Intercept an event call on the underlying statement object.
+		 * If the method represents a statement execution, a behavior tracking event will
+		 * be recorded, including any event data gathered from preceding calls to
+		 * {@link #addBatch(EventDataElement, Object[])}, {@link #addExecuteParameters(BehaviorEvent, Object[])},
+		 * and {@link #addOperationData(String, Object[])}.
+		 */
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 			Object stub = invokeIdentity(proxy, method, args);
 			if (stub != null)
@@ -192,7 +209,7 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 				String op = method.getName();
 				if (op.startsWith("execute")) {
 					//query or batch is being executed -- start the event timer.
-					BehaviorEvent event = start(op, findSqlArg(args));
+					BehaviorEvent event = start(op, args);
 					try {
 						return invokeTarget(method, args);
 					} catch (Throwable t) {
@@ -207,71 +224,150 @@ public class BehaviorTrackingDataSource extends DelegatingDataSource {
 					//add parameter and sql info to event data.
 					if (batch == null)
 						batch = new EventDataElement("batch");
-
-					String sql = findSqlArg(args);
-					if (sql != null) {
-						//static SQL batch. add an element to contain SQL.
-						batch.addElement("sql").setText(sql);
-					} else {
-						//prepared statement batch.  add any parameters to 
-						//event info and reset for next statement.
-						batch.addElement(parameters);
-						parameters = new EventDataElement("parameters");
-					}
-					
-				} else if (op.startsWith("set")) {
-					//maybe parameters being set?  store up parameters in hash table until we start another execute event.
-					//we have to support multiple execute() calls on the same statement object to support prepared / callable API
-					Object key = null;
-					Object value = null;
-					if (op.equals("setNull")) {
-						key = args[0];
-					} else if (args.length == 2) {
-						if (!(op.endsWith("Stream") || op.endsWith("lob"))) {
-							key = args[0];
-							value = args[1];
-						}
-					} else if (args.length == 3 && ("setObject".equals(op) || "setDate".equals(op))) {
-						key = args[0];
-						value = args[1];
-					}
-					
-					if (key != null) {
-						EventDataElement v = parameters.addElement("param");
-						if (value != null)
-							v.setText(value.toString());
-					}
+					addBatch(batch, args);
+				} else {
+					addOperationData(op, args);
 				}
 			}
 			
 			return invokeTarget(method, args);
 		}
-
-		private BehaviorEvent start(String name, String sql) {
-			
-			//initialize statement event with the accumulated parameter and SQL information.
-			BehaviorEvent event = trackingManager.createEvent(eventType, name);
-			EventDataElement data = event.addData();
-
-			//SQL could either be provided as parameter, or when statement was created.
-			String actualSql = sql == null ? this.sql : sql;
-			if (actualSql != null) 
-				data.addElement("sql").setText(actualSql);
-
+		
+		/**
+		 * Hook for subclasses to extract any data from a Statement method call that is
+		 * not an execution.  E.g. prepared statements receive parameter data from
+		 * various setXX() calls.  Default behavior does nothing.
+		 */
+		protected void addOperationData(String op, Object[] args) {
+		}
+		
+		/** 
+		 * Create and start a tracking event, corresponding to an executeXX() method call on
+		 * a statement object.
+		 * @param name the name of the execute method (e.g. execute, executeUpdate, executeBatch, etc).  {@link #createEvent(String)} will be called with this argument.
+		 * @param args the arguments passed to the execute method.  {@link #addExecuteParameters(BehaviorEvent, Object[])} will be called for non-batch executions.
+		 */
+		private BehaviorEvent start(String name, Object[] args) {
+			BehaviorEvent event = createEvent(name);
 			if (name.endsWith("Batch")) {
+				//consolidate batch call execution data into root data element.
 				if (batch != null) {
-					data.addElement(batch);
+					event.addData().addElement(batch);
 					batch = null;
 				}
-			} else if (!parameters.isEmpty()) {
-				//transfer parameters from buffer into event object.
-				data.addElement(parameters);
-				//clear out the parameter buffer for the next execute event.
-				parameters = new EventDataElement("parameters");
+			} else {
+				addExecuteParameters(event, args);
 			}
 
 			trackingManager.start(event);
 			return event;
 		}
+		
+		/**
+		 * Create, but do not start, a new behavior tracking event for the given execute method name.
+		 * Simply calls {@link BehaviorTrackingManager#createEvent(String, String)}.
+		 */
+		protected BehaviorEvent createEvent(String name) {
+			//initialize statement event with the accumulated parameter and SQL information.
+			return trackingManager.createEvent(eventType, name);
+		}
 	}
+	
+	/**
+	 * Behavior tracking logic for prepared and callable statements.
+	 */
+	protected class PreparedStatementHandler extends StatementHandler {
+
+		private String sql;
+		private EventDataElement parameters = new EventDataElement("parameters");
+		
+		public PreparedStatementHandler(ConnectionHandler parent, Object target, String sql) {
+			super(parent, target);
+			this.sql = sql;
+		}
+
+		/** overridden to append the prepared statement SQL to the newly created event */
+		@Override
+		protected BehaviorEvent createEvent(String name) {
+			BehaviorEvent ret = trackingManager.createEvent(eventType, name);
+			ret.addData().addElement("sql").setText(sql);
+			return ret;
+		}
+
+		/** overridden to add prepared statement parameter data to the batch data element */
+		@Override
+		protected void addBatch(EventDataElement batchData, Object[] args) {
+			//prepared statement batch.  add any parameters to 
+			//event info and reset for next statement.
+			batchData.addElement(parameters);
+			parameters = new EventDataElement("parameters");
+		}
+
+		/** overridden to add prepared statement parameter data to the execute event */
+		@Override
+		protected void addExecuteParameters(BehaviorEvent event, Object[] args) {
+			if (!parameters.isEmpty()) {
+				//transfer parameters from buffer into event object.
+				event.addData().addElement(parameters);
+				//clear out the parameter buffer for the next execute event.
+				parameters = new EventDataElement("parameters");
+			}
+		}
+
+		/**
+		 * Overridden to read any prepared statement parameter info out of the given method call data,
+		 * for inclusion in the next {@link #addBatch(EventDataElement, Object[])} or {@link #addExecuteParameters(BehaviorEvent, Object[])}
+		 * call.
+		 */
+		@Override
+		protected void addOperationData(String op, Object[] args) {
+			//maybe parameters being set?  store up parameters in 'parameters' element until we start another execute event or batch statement.
+			//we have to support multiple execute() calls on the same statement object to support prepared / callable API
+			if (op.startsWith("set")) {
+				Object key = null;
+				Object value = null;
+				if (op.equals("setNull")) {
+					key = args[0];
+				} else if (args.length == 2) {
+					if (!(op.endsWith("Stream") || op.endsWith("lob"))) {
+						key = args[0];
+						value = args[1];
+					}
+				} else if (args.length == 3 && ("setObject".equals(op) || "setDate".equals(op))) {
+					key = args[0];
+					value = args[1];
+				}
+				
+				if (key != null) {
+					EventDataElement v = parameters.addElement("param");
+					if (value != null)
+						v.setText(value.toString());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Behavior tracking logic for dynamic (not prepared or callable) sql statements.
+	 */
+	protected class DynamicStatementHandler extends StatementHandler {
+
+		public DynamicStatementHandler(ConnectionHandler parent, Object target) {
+			super(parent, target);
+		}
+		
+		/** overridden to add the static sql from <code>args</code> to the event data */
+		@Override
+		protected void addExecuteParameters(BehaviorEvent event, Object[] args) {
+			event.addData().addElement("sql").setText(findSqlArg(args));
+		}
+
+		/** overridden to add the static sql from <code>args</code> to the event data */
+		@Override
+		protected void addBatch(EventDataElement batchData, Object[] args) {
+			//static SQL batch. add an element to contain SQL.
+			batchData.addElement("sql").setText(findSqlArg(args));
+		}
+	}
+	
 }
