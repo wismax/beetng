7d89a493896297946b24135a39b154e288d28bb8
jason.trump <jason.trump@mantis-tgi.com>
2009-04-20 22:45:48 -0700

    minor tweaks to layout following rollout to sf.net

--- src/site/templates/root/index.php
+++ src/site/templates/root/index.php
@@ -38,7 +38,7 @@
 
 					?><div id="rootHeaderLeft">
 						<h1 class="<?php echo $logoClass; ?>">
-							<a href="index.php" title="<?php echo $siteName; ?>"><?php echo $logoText; ?></a>
+							<a href="/" title="<?php echo $siteName; ?>"><?php echo $logoText; ?></a>
 						</h1>
 						<?php if(JRequest::getCmd( 'view' ) != 'frontpage') : ?>
 						<div id="rootBreadcrumbs">
